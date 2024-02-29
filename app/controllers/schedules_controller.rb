81e526e315de46845c33dcb166995437a7ca72bd
中村康太 <nakamurakouta@koutaMacBook-Pro.local>
2024-02-29 23:14:37 +0900

    編集機能・削除機能を実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -22,12 +22,24 @@ class SchedulesController < ApplicationController
   end
 
   def edit
+    @schedule = Schedule.find(params[:id])
   end
 
   def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(params.require(:schedule).permit(:title, :start_date, :end_date, :allday, :memo))
+      flash[:notice] = "スケジュールIDが「#{@schedule.id}」の情報を更新しました"
+      redirect_to :schedules
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    flash[:notice] = "予定を削除しました"
+    redirect_to :schedules
   end
 
 end
